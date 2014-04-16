d9ed40b8ad31f4f154706da89323f5c0eb0c5975
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-04-16 16:52:33 +0200

    Add readme

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 # If False the server is only accessible from your own computer.
 PUBLIC = False
 
 # The port the webserver will run on.
 PORT = 5000
 
-# The directly where the QuakeML files are located. The script will search
+# The directory where the QuakeML files are located. The script will search
 # recursively through all subfolders.
 QUAKEML_ROOT_DIR = "../../data/2014_04_15--VERCE_GCMT_Catalog/quakemls/"
 
-# A glob expression to find the actual QuakeML files.
+# A glob expression to find the actual QuakeML files. Usually "*.xml" or "*
+# .qml".
 QUAKEML_FILES_GLOB = "*.xml"
 
-# The location of the shelve file to store the event information.
+# The location of the shelve file to store the event information. The event
+# indices are stored there so they do not have to be created each time the
+# server starts.
 SHELVE_DB_PATH = "event_db.shelve"
 
 # The regex used on the resource id of the event to extract the event id. If
@@ -23,4 +23,6 @@ SHELVE_DB_PATH = "event_db.shelve"
 # event_id = re.match(REGEX_FOR_EVENT_ID, resource_id).group(1)
 #
 # The example will extract 'B061690B' from 'smi:local/ndk/B061690B/event'
+#
+# The event id is only useful when wanting to request an event via its id.
 REGEX_FOR_EVENT_ID = r"\w+\:\w+\/\w+\/(\w+)"
